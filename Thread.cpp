8f1db6c8476e5bc4e85a85a1733ef4fc2cd145ae
zhangkuo <vczk@qq.com>
2017-10-17 15:09:25 +0800

    add Thread

---
+++ Thread.cpp
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include "Thread.h"
+
+std::atomic<int> Thread::numCreated_ = 0;
+
+Thread::Thread(const ThreadFunc& func, const std::string& name):
+	func_(func),
+	name_(name),
+	started_(false),
+	joined_(false)
+{
+	numCreated_++;
+	if (name_.empty())
+	{
+		char buf[32];
+		_snprintf_s(buf, sizeof buf, "Thread%d", numCreated_);
+		name_ = buf;
+	}
+}
+
+Thread::Thread(ThreadFunc&& func, const std::string& name):
+	func_(std::move(func)),
+	name_(name),
+	started_(false),
+	joined_(false)
+{
+	numCreated_++;
+	if (name_.empty())
+	{
+		char buf[32];
+		_snprintf_s(buf, sizeof buf, "Thread%d", numCreated_);
+		name_ = buf;
+	}
+}
+
+Thread::~Thread()
+{
+	if (started_ && !joined_)
+	{
+		pThread_->detach();
+	}
+}
+
+void Thread::start()
+{
+	started_ = true;
+	pThread_ = std::make_shared<std::thread>(func_);
+}
+
+void Thread::join()
+{
+	joined_ = true;
+	pThread_->join();
+}
