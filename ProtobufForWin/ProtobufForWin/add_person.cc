dbd374f769bd7c39d7627815f39345aebfd4b632
zhangkuo <vczk@qq.com>
2017-12-27 14:13:32 +0800

    update

--- ProtobufForWin/ProtobufForWin/add_person.cc
+++ ProtobufForWin/ProtobufForWin/add_person.cc
@@ -5,98 +5,47 @@
 #include <google/protobuf/util/time_util.h>
 #include <iostream>
 #include <string>
+#include <google/protobuf/descriptor.h>
 
 #include "proto/addressbook.pb.h"
 
 using namespace std;
+using namespace google::protobuf;
 
 using google::protobuf::util::TimeUtil;
 
-// This function fills in a Person message based on user input.
-void PromptForAddress(tutorial::Person* person) {
-  cout << "Enter person ID number: ";
-  int id;
-  cin >> id;
-  person->set_id(id);
-  cin.ignore(256, '\n');
 
-  cout << "Enter name: ";
-  getline(cin, *person->mutable_name());
-
-  cout << "Enter email address (blank for none): ";
-  string email;
-  getline(cin, email);
-  if (!email.empty()) {
-    person->set_email(email);
-  }
-
-  while (true) {
-    cout << "Enter a phone number (or leave blank to finish): ";
-    string number;
-    getline(cin, number);
-    if (number.empty()) {
-      break;
-    }
-
-    tutorial::Person::PhoneNumber* phone_number = person->add_phones();
-    phone_number->set_number(number);
-
-    cout << "Is this a mobile, home, or work phone? ";
-    string type;
-    getline(cin, type);
-    if (type == "mobile") {
-      phone_number->set_type(tutorial::Person::MOBILE);
-    } else if (type == "home") {
-      phone_number->set_type(tutorial::Person::HOME);
-    } else if (type == "work") {
-      phone_number->set_type(tutorial::Person::WORK);
-    } else {
-      cout << "Unknown phone type.  Using default." << endl;
-    }
-  }
-  *person->mutable_last_updated() = TimeUtil::SecondsToTimestamp(time(NULL));
-}
-
-// Main function:  Reads the entire address book from a file,
-//   adds one person based on user input, then writes it back out to the same
-//   file.
 int main(int argc, char* argv[]) {
-  // Verify that the version of the library that we linked against is
-  // compatible with the version of the headers we compiled against.
   GOOGLE_PROTOBUF_VERIFY_VERSION;
 
-  /*if (argc != 2) {
-    cerr << "Usage:  " << argv[0] << " ADDRESS_BOOK_FILE" << endl;
-    return -1;
-  }*/
+  string typeName = tutorial::Person::descriptor()->full_name();
+  cout << "typename: " << typeName << endl;
 
-  tutorial::AddressBook address_book;
+  const Descriptor* descriptor = DescriptorPool::generated_pool()->FindMessageTypeByName(typeName);
+  cout << "FindMessageTypeByName() = " << descriptor << endl;
+  cout << "Person::descriptor() = " << tutorial::Person::descriptor() << endl;
+  cout << endl;
 
-  {
-    // Read the existing address book.
-    fstream input("book.data", ios::in | ios::binary);
-    if (!input) {
-      cout << "book.data" << ": File not found.  Creating a new file." << endl;
-    } else if (!address_book.ParseFromIstream(&input)) {
-      cerr << "Failed to parse address book." << endl;
-      return -1;
-    }
-  }
+  const Message* protoType = MessageFactory::generated_factory()->GetPrototype(descriptor);
+  cout << "GetPrototype() = " << protoType << endl;
+  cout << "Person::default_instance() = " << &tutorial::Person::default_instance() << endl;
+  cout << endl;
 
-  // Add an address.
-  PromptForAddress(address_book.add_people());
+  tutorial::Person* obj = (tutorial::Person*)(protoType->New());
+  assert(typeid(*obj) == typeid(tutorial::Person::default_instance()));
+  cout << "protoType->New() = " << obj << endl;
+  delete obj;
 
-  {
-    // Write the new address book back to disk.
-    fstream output("book.data", ios::out | ios::trunc | ios::binary);
-    if (!address_book.SerializeToOstream(&output)) {
-      cerr << "Failed to write address book." << endl;
-      return -1;
-    }
-  }
+  tutorial::Person person;
+  person.set_id(123456789);
+  person.set_name("zhangkuo");
+  person.set_email("vczk@foxmail.com");
 
-  // Optional:  Delete all global objects allocated by libprotobuf.
-  google::protobuf::ShutdownProtobufLibrary();
+  cout << person.DebugString() << endl;
+  cout << person.SerializeAsString() << endl;
+  cout << person.SerializePartialAsString() << endl;
+  
+  //cout << person.Serialize
 
   system("pause");
   return 0;
